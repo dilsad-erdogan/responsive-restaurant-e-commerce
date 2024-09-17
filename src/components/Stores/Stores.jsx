96dcc3c79ebe295802772aad8e2cdc93f2c9e2d8
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-17 20:10:54 +0300

    stores add

--- src/components/Stores/Stores.jsx
+++ src/components/Stores/Stores.jsx
@@ -1,8 +1,23 @@
-
+import playStore from "../../assets/play_store.png";
+import ipStore from "../../assets/app_store.png";
+import gif from "../../assets/mobile_bike.gif";
 
 const Stores = () => {
   return (
-    <div>Stores</div>
+    <div className="bg-gray-200 justify-center items-center flex m-8">
+      <div className="p-5 m-5 max-w-[400px]">
+        <h1 className="font-bold text-2xl">Foodly is available for Android and IOS</h1>
+
+        <div className="flex items-center justify-start pt-6">
+          <img src={playStore} alt="" className="max-w-[150px]" />
+          <img src={ipStore} alt="" className="max-w-[150px]" />
+        </div>
+      </div>
+
+      <div className="p-5 m-5 max-w-[300px]">
+        <img src={gif} alt="" />
+      </div>
+    </div>
   )
 }
 
