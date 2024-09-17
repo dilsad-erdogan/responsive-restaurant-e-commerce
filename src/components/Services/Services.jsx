b66406a3ea17658b686cf53046c4d6ef5cd9840b
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-17 18:00:11 +0300

    Services add

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,8 +1,42 @@
+import Heading from "../Shared/Heading"
+import img from "../../assets/biryani2.png";
 
+const ServicesData = [
+  {
+    id: 1,
+    title: "Biryani",
+    subtitle: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis delectus architecto error nesciunt.",
+    img: img,
+  },
+  {
+    id: 2,
+    title: "Chiken Kari",
+    subtitle: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis delectus architecto error nesciunt.",
+    img: img,
+  },
+  {
+    id: 3,
+    title: "Cold Cofee",
+    subtitle: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis delectus architecto error nesciunt.",
+    img: img,
+  },
+];
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="justify-center items-center">
+      <Heading firsttitle={"Our Services"} title={"Services"} subtitle={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis delectus architecto error nesciunt."}/>
+
+      <div className="p-5 flex justify-center items-center gap-6">
+        {ServicesData.map((data) => (
+          <div key={data.id}>
+            <img src={data.img} alt="" className="max-h-[250px]" />
+            <h1 className="text-2xl font-bold text-center pt-3 pb-2">{data.title}</h1>
+            <p className="text-xs text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
